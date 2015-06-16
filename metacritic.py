19c0991c0cf6efb0ff9868229df087361259b7e2
Shashank Ramaprasad <shashank.ramaprasad@gmail.com>
2015-06-15 23:41:54 -0700

    semi-major rewrite: vectorize more operations, better handling of missing ratings

--- metacritic.py
+++ metacritic.py
@@ -46,18 +46,13 @@ def get_critics(many_ratings):
             critics[c] += 1
     return critics
 
-def calc_overall_rating(weights, critic_ratings):
-    unnormalized = sum(weights[critic] * score for critic, score in critic_ratings.items())
-    total_weight = sum(weights[critic] for critic in critic_ratings)
-    return unnormalized/total_weight
-
 def err(actual, predicted):
     return (predicted - actual) * 1.0 /actual
 
 def show_accuracy(url, actual, predicted, error_pct, ratings):
     print "[p: %3.2f, a: %3.2f, e: %3.2f] %s" % (predicted, actual, error_pct, url)
 
-def extract_training_set(urls, training_pct):
+def extract_training_keys(urls, training_pct):
     frac = training_pct / 100.
     return set([url for url in urls if random() < frac])
 
@@ -65,15 +60,18 @@ def rmse(errors):
     n = len(errors)
     return sqrt(sum(e * e for e in errors))/n if n else 0.
 
-def train_and_test(movie_ratings, training_pct, tech):
+def train_and_test(ratings_data, training_pct, tech):
     """
     """
-    training_set = extract_training_set(movie_ratings, training_pct)
-    all_critics = get_critics(movie_ratings.values())
-    predicted_weights = infer_weights([movie_ratings[u]
-                                       for u in training_set
-                                       if u in movie_ratings],
-                                      all_critics,
+    # --- 1. list of all critics
+    # convert to list for consistent enumeration
+    all_critics = get_critics(ratings_data.values())
+    # --- 2. extract training set
+    training_keys = extract_training_keys(ratings_data, training_pct)
+    training_data = [ratings for url, ratings in ratings_data.items()
+                     if url in training_keys]
+    predicted_weights = infer_weights(training_data,
+                                      list(all_critics),
                                       tech)
     pretty_print_weights(all_critics, predicted_weights)
     test_set = set(movie_ratings).difference(training_set)
@@ -94,25 +92,10 @@ def train_and_test(movie_ratings, training_pct, tech):
     print "RMSE: %.6f" % (m,)
     print "**********************"
 
-def build_A(multiple_ratings, critic_index):
-    num_critics = len(critic_index)
-    num_movies = len(multiple_ratings)
-    A = np.zeros((num_movies, num_critics))
-    total_ratings = 0
-    for i, (overall, ratings) in enumerate(multiple_ratings):
-        for name, rating_ij in ratings.items():
-            j = critic_index[name]
-            # normalized rating
-            A[i, j] =  rating_ij - overall
-            total_ratings += 1
-    return A, total_ratings
-
-def infer_weights(multiple_ratings, all_critics, tech):
+def infer_weights(training_data, all_critics, tech):
     """Return a dictionary mapping critics to relative weights.
 
-    all_critics: a set of critic names;
-
-    multiple_ratings: is a list of data, one per movie, of the form:
+    training_data: is a list of data, one per movie, of the form:
 
         (overall_score, critic_scores)
 
@@ -120,47 +103,50 @@ def infer_weights(multiple_ratings, all_critics, tech):
 
         critic_name -> critic rating for the movie.
 
-    Uses linear regression to find the best fitting weights;
-    notice that the linear system might be overdetermined if:
-
-        num_movies > num_critics.
+    all_critics: an ordered list of critic names;
     """
 
     assert tech in TECHNIQUES
 
-    num_critics = len(all_critics)
-    num_movies = len(multiple_ratings)
-    # critic name -> critic number
-    critic_index = dict((c, i) for i, c in enumerate(all_critics))
-    A, total_ratings = build_A(multiple_ratings, critic_index)
-    print ">>>> %s with %d ratings of %d movies by %d critics" % (tech,
-                                                                  total_ratings,
-                                                                  num_movies,
-                                                                  num_critics)
-    print A
-
-    # min ||AX|| (optionally with X >= 0 and sum X = 1)
-
-    # 1. objective function, that needs to be minimized
+    # --- 1. calculate the r_dash matrix
+    r_dash = []
+    for _, ratings in training_data:
+        r_dash.append([ratings.get(c, 0) for c in all_critics])
+    r_dash = np.matrix(r_dash)
+    # --- 2. calculate the e matrix
+    e_matrix = []
+    for _, ratings in training_data:
+        e_matrix.append([1 if c in ratings else 0 for c in all_critics])
+    e_matrix = np.matrix(e_matrix)
+    # --- 3. construct the p vector
+    p_vector = np.array([overall for overall, _ in training_data])
+    # --- 4. the actual model
+    m = len(training_data)
+    n = len(all_critics)
+    # all theta values are positive
+    bounds = [(1e-9, 1)] * n         # min, max
+    theta0 = [1.0/n] * n             # initial values
+    def y(theta):
+        theta = np.transpose(np.asmatrix(theta))
+        numerator = r_dash * theta   # each of these is an m-vector:
+        denom = e_matrix * theta     # (m x n) times (n x 1).
+        # element-wise division:
+        # denom elements are _guaranteed_ to be positive, because:
+        #   - theta values are always positive, and
+        #   - e values are non-negative, and never all zeros.
+        # so we should never see a divide by zero.
+        return numerator / denom
+    def d(theta):
+        return p_vector - y(theta)
     def obj_f(theta):
-        return LA.norm(A.dot(theta))
-
-    # 2. initial, random solution: XXX what if this is not feasible?
-    theta0 = np.array([random() for i in range(num_critics)])
-
-    # 3. bounds
-    bounds = [(0, 1) for i in range(num_critics)]
-
-    # 4. constraints
+        return LA.norm(d(theta))
     constraints = {'type': 'eq', 'fun': lambda theta: sum(theta) - 1}
-
-    res = sci.minimize(obj_f, theta0, method=tech, bounds=bounds, constraints=constraints)
-    result = dict((name, res.x[i]) for name, i in critic_index.items())
-    if not res.success:
-        msg = res.message
-        error( ">>>> Current result:" )
-        error( result )
+    # --- 5. actual call to optimize
+    result = sci.minimize(obj_f, theta0, bounds=bounds, constraints=constraints, method=tech)
+    if not result.success:
+        msg = result.message
         raise Exception("unable to converge to a solution using %(tech)s: %(msg)s" % locals())
+    result = dict((name, result.x[i]) for i, name in enumerate(all_critics))
     return result
 
 def pretty_print_weights(all_critics, weights):
@@ -203,7 +189,7 @@ if __name__ == '__main__':
                            help="what percentage of data should be used to train?")
     options, args = args_parser.parse_args()
     if options.test_only:
-        testitout('COBYLA')
+        testitout(options.tech)
         exit(0)
     if not options.train_with:
         args_parser.error("how much data to train with?")
